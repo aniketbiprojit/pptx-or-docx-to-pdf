b8ddb5f983fcdceb48c8f4e3170514d416aa18ea
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-04-26 19:56:36 +0530

    ordered

--- run.py
+++ run.py
@@ -13,12 +13,17 @@ def PPTtoPDF(inputFileName, outputFileName, formatType=32):
     print('done')
     powerpoint.Quit()
 
+search_dir = "./"
+os.chdir(search_dir)
+files = filter(os.path.isfile, os.listdir(search_dir))
+files = [os.path.join(search_dir, f) for f in files] # add path to each file
+files.sort(key=lambda x: os.path.getmtime(x))
 
-files = []
-for (dirpath, dirnames, filenames) in walk('./'):
-    files.extend(filenames)
-    break
-
+# for (dirpath, dirnames, filenames) in walk('./'):
+#     files.extend(filenames)
+#     break
+files.reverse()
+print(files)
 for file in files:
     if(file[-4:] == 'pptx'):
         print(file)
